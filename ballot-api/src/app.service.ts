41060bca5d5bd87a8c8dd5a1d61bf6bf65e35c28
Bastien NICOLAU <nicolau.bastien@proton.me>
2023-11-16 22:30:10 +0100

    Add required dependencies

--- ballot-api/src/app.service.ts
+++ ballot-api/src/app.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ethers } from 'ethers';
 
 @Injectable()
 export class AppService {
+  contract: ethers.Contract;
+  provider: ethers.Provider;
+  wallet: ethers.Wallet;
+
+  constructor(private configService: ConfigService) {
+    this.provider = new ethers.JsonRpcProvider(
+      this.configService.get<string>('RPC_ENDPOINT_URL'),
+    );
+    this.wallet = new ethers.Wallet(
+      this.configService.get<string>('PRIVATE_KEY'),
+      this.provider,
+    );
+    this.contract = new ethers.Contract(
+      this.configService.get<string>('TOKEN_ADDRESS'),
+      '@TODO tokenJson.abi',
+      this.wallet,
+    );
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
